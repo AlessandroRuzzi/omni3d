7cead68e69d62a350971d2664ee7ce0b742076d8
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-02-01 12:08:53 +0100

    u

--- pre_process_behave.py
+++ pre_process_behave.py
@@ -115,7 +115,7 @@ for id_data,dl in enumerate([(train_dl,"Train"), (val_dl,"Validation"), (test_dl
                             "bbox2D_proj"	  : patch_coord_projected.tolist(),			# 2D corners projected from bbox3D
                             "bbox2D_trunc"	  : [],			# 2D corners projected from bbox3D then truncated
                             "bbox3D_cam"	  : bbox_corners.tolist(),
-                            "center_cam"	  : [bbox[0,:3]].tolist(),				
+                            "center_cam"	  : [bbox[0,:3]],				
                             "dimensions"	  : [bbox[0,3], bbox[0,3], bbox[0,3]],
                             "R_cam"		      : np.eye(3).tolist(),	
 
