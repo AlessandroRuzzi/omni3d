c81ee79d7b29ef4a79aaaefe8f12de9eaa22047c
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-02-03 10:02:03 +0100

    u

--- pre_process_behave.py
+++ pre_process_behave.py
@@ -24,7 +24,6 @@ def plot_box_and_label(image, lw, box, label='', color=(128, 128, 128), txt_colo
         cv2.putText(image, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), font, lw / 3, txt_color,
                     thickness=tf, lineType=cv2.LINE_AA)
 
-@staticmethod
 def generate_colors(i, bgr=False):
     hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
             '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
