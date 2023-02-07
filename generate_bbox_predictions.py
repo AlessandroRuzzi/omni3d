cdf7daef200b236c0a427113a52a3a227bf8bcb4
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-02-07 12:24:10 +0100

    u

--- generate_bbox_predictions.py
+++ generate_bbox_predictions.py
@@ -25,6 +25,7 @@ from cubercnn.modeling.roi_heads import ROIHeads3D
 from cubercnn.modeling.meta_arch import RCNN3D, build_model
 from cubercnn.modeling.backbone import build_dla_from_vision_fpn_backbone
 from cubercnn import util, vis
+import json
 
 from tqdm import tqdm
 
@@ -32,7 +33,7 @@ def do_test(args, cfg, model):
 
     import json
     
-    with open('/data/aruzzi/Behave/Behave_test.json', 'r') as f:
+    with open('/data/aruzzi/Behave/info.json', 'r') as f:
         info = json.load(f)
 
     list_of_ims = info['img_paths'] # util.list_files(os.path.join(args.input_folder, ''), '*.jpg')
@@ -63,12 +64,12 @@ def do_test(args, cfg, model):
     
     for bbox, path in zip(list_of_bbx, tqdm(list_of_ims)):
 
-        im_name = path[len('/home/cluster/workshop/data/behave/sequences/'):]
+        im_name = path[len('/data/xiwang/behave/sequences/'):]
 
         im = util.imread(path)
         
-        #bbox[0] *= -1
-        #bbox[1] *= -1
+        bbox[0] *= -1
+        bbox[1] *= -1
 
         if im is None:
             continue
@@ -126,7 +127,81 @@ def do_test(args, cfg, model):
                 "bbox_class": [cats[c] for c in dets.pred_classes.tolist()],
                 "bbox_orientation": dets.pred_pose.tolist()
             }
+    with open('predictions/results.json', 'w') as f:
+        json.dump({"best_score vs gt": res, "all_predicted": all_predicted}, f)
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    get_cfg_defaults(cfg)
+
+    config_file = args.config_file
+
+    # store locally if needed
+    if config_file.startswith(util.CubeRCNNHandler.PREFIX):    
+        config_file = util.CubeRCNNHandler._get_local_path(util.CubeRCNNHandler, config_file)
+
+    cfg.merge_from_file(config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+def main(args):
+    cfg = setup(args)
+    model = build_model(cfg)
+    
+    logger.info("Model:\n{}".format(model))
+    DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+        cfg.MODEL.WEIGHTS, resume=True
+    )
+
+    with torch.no_grad():
+        do_test(args, cfg, model)
 
 if __name__ == "__main__":
-    do_test()
+    
+    parser = argparse.ArgumentParser(
+        epilog=None, formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument("--config-file", default="/configs/base_Behave.yaml", metavar="FILE", help="path to config file")
+    parser.add_argument('--input-folder',  type=str, help='list of image folders to process', required=True)
+    parser.add_argument("--threshold", type=float, default=0.25, help="threshold on score for visualizing")
+    parser.add_argument("--display", default=False, action="store_true", help="Whether to show the images in matplotlib",)
+    
+    parser.add_argument("--eval-only", default=True, action="store_true", help="perform evaluation only")
+    parser.add_argument("--num-gpus", type=int, default=1, help="number of gpus *per machine*")
+    parser.add_argument("--num-machines", type=int, default=1, help="total number of machines")
+    parser.add_argument(
+        "--machine-rank", type=int, default=0, help="the rank of this machine (unique per machine)"
+    )
+    port = 2 ** 15 + 2 ** 14 + hash(os.getuid() if sys.platform != "win32" else 1) % 2 ** 14
+    parser.add_argument(
+        "--dist-url",
+        default="tcp://127.0.0.1:{}".format(port),
+        help="initialization URL for pytorch distributed backend. See "
+        "https://pytorch.org/docs/stable/distributed.html for details.",
+    )
+    parser.add_argument(
+        "opts",
+        help="Modify config options by adding 'KEY VALUE' pairs at the end of the command. "
+        "See config references at "
+        "https://detectron2.readthedocs.io/modules/config.html#config-references",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+
+    args = parser.parse_args()
+
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
 
