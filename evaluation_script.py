0259caa38be8b0d5c93e78deaa6cb13834f4bd14
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-02-08 15:54:46 +0100

    u

--- evaluation_script.py
+++ evaluation_script.py
@@ -77,7 +77,9 @@ def calc_errors_on_high_prob_bbox(results):
     error_dict = {'x' : 0, 'y' : 0, 'z': 0, 'l': 0 , 'num_imgs' : 0}
 
     for day in results:
+        print(day)
         pred_dict = results[day]
+        print(pred_dict)
         gt_box = pred_dict["gt_bbox_center"]
         pred_box = pred_dict["pred_bbox_center"]
         gt_length = pred_dict["gt_bbox_size"][0]
