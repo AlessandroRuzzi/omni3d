fb801dc07404928953b46d7dc7409db0d49265b0
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-02-08 11:15:31 +0100

    u

--- generate_json.py
+++ generate_json.py
@@ -20,7 +20,8 @@ for index, i in enumerate(dataset):
     # copy the image (i['img_path']) to a new folder (/home/cluster/workshop/omni3d/datasets/miniBEHAVE)
     # name = i['img_path'][len('/home/cluster/workshop/data/behave/sequences/'):]
     # name = name.replace('/', '_')
-    #print(index)
+    if index % 1000 == 0:
+        print(index)
     
     bbox.append(i['bbox'].tolist())
     img_paths.append(i['img_path'])
