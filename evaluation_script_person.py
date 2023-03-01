5c386a043f9c307cdf7afb25f947675f59e69f4a
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-03-01 08:22:40 +0100

    u

--- evaluation_script_person.py
+++ evaluation_script_person.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     calc_errors_on_closest_bbox_human(results, results_all, human_pare_all)
 
-    calc_errors_on_closest_bbox_human_by_class(results, results_all, human_pare_all)
+    #calc_errors_on_closest_bbox_human_by_class(results, results_all, human_pare_all)
 
     calc_num_wrong_bbox(results)
 
