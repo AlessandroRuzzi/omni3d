ef7ed7550a6c754df2995e1c64790429c2a32d59
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-03-18 11:35:12 +0100

    u

--- evaluation_script_person.py
+++ evaluation_script_person.py
@@ -48,7 +48,7 @@ def log_bboxes(img, object_box, object_dim, object_orientation, object_cat, obje
 
         meshes = []
         meshes_text = []
-        bbox3D = object_box.tolist() + object_dim.tolist()
+        bbox3D = object_box + object_dim
         meshes_text.append('{} {:.2f}'.format(object_cat, object_score))
         color = [c/255.0 for c in util.get_color(id_cat)]
         box_mesh = util.mesh_cuboid(bbox3D, object_orientation.tolist(), color=color)
