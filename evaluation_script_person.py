3dd3254425ca7f36cd979abebb2ec976d03541e7
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-03-19 11:43:04 +0100

    u

--- evaluation_script_person.py
+++ evaluation_script_person.py
@@ -121,7 +121,12 @@ def log_bboxes(img,img_name, object_box, object_dim, object_orientation, object_
         wandb.log({"Pred BBox" : images})
 
         print(im_topdown.shape)
-        final_log_image = Image.fromarray(np.concatenate([im_drawn_rgb, im_topdown], axis=1))
+        tmp_img = np.concatenate([im_drawn_rgb, im_topdown], axis=1)
+        print(tmp_img.shape)
+        print(tmp_img[0].shape)
+        final_log_image = Image.fromarray(tmp_img)
+        images = wandb.Image(final_log_image, caption="Topdown image with predicted 3D bounding boxes")
+        wandb.log({"Full Image" : images})
         if final_log_image.mode != 'RGB':
             final_log_image = final_log_image.convert('RGB')
 
