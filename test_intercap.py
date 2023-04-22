8045867693cb35811578c022d2fd42f899936809
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-04-22 14:42:42 +0200

    u

--- test_intercap.py
+++ test_intercap.py
@@ -60,11 +60,6 @@ def log_bboxes(img,day, object_box, object_dim, object_orientation, object_cat, 
         
         kid = day
         K = intrinsics[kid]
-        K = np.array([
-            [976.2120971679688, 0.0, 1017.9580078125], 
-            [0.0, 976.0467529296875, 787.3128662109375], 
-            [0.0, 0.0, 1.0]
-        ])
         color = util.get_color(id_cat)
 
         meshes = []
@@ -128,6 +123,11 @@ def test_intercap(args, cfg, model):
             [0.0, 976.0467529296875, 787.3128662109375], 
             [0.0, 0.0, 1.0]
         ])
+
+        intrinsics = [load_intrinsics("dataset/calibration_intercap", i+1) for i in range(6)]
+
+        K = intrinsics[0]
+        print(K)
 
         batched = [{
             'image': torch.as_tensor(np.ascontiguousarray(img.transpose(2, 0, 1))).cuda(), 
