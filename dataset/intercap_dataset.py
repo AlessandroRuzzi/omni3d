b5123b228b034a1f61d3c743bb421444d16616cf
AlessandroRuzzi <alrxruzzi98@gmail.com>
2023-05-24 19:52:37 +0200

    u

--- dataset/intercap_dataset.py
+++ dataset/intercap_dataset.py
@@ -267,6 +267,6 @@ def load_smpl(smpl_paths, seg, frame, kid):
     #jtr = jtr[0].detach().numpy()
     
     r, t = iu.get_rotation_translation(intercap_calib, seg, kid)
-    jtr = (r@jtr.T ).T + t
+    jtr = (r@jtr.T ).T - t
     
     return jtr.detach().cpu()
